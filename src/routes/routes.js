7b7079e4e3a34725c9db4813dd53e060b05934e2
Hyeeun <hyeeuni.mok@gmail.com>
2019-09-22 11:33:54 -0400

    creating activity page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import LandingPage from '../pages/landingPage';
+import Activity from '../pages/activityPage';
 import Alert from '../pages/alertPage';
 import AlertInfo from '../pages/alertInfoPage';
 import StoreBuy from '../pages/storePage';
@@ -12,6 +13,7 @@ class AppRouter extends React.Component {
     return (
       <BrowserRouter>
         <Route path="/landing" component={LandingPage} />
+        <Route path="/activity" render={(props) => <Activity {...props} />} />
         <Route path="/alerts" render={(props) => <Alert {...props} />} />
         <Route path="/alertinfo" render={(props) => <AlertInfo {...props} />} />
         <Route path="/storebuy" render={(props) => <StoreBuy {...props} />} />
